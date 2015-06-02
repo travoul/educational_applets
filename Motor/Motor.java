3cfba757b69f299742a176a16b475ae4ba13d4b4
Henrique Silveira <hd2312@columbia.edu>
2015-06-02 18:55:25 -0300

    Criei o projeto do motor

---
+++ Motor/Motor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package motor;
+import java.util.Random;
+/**
+ *
+ * @author henrisilver
+ */
+public class Motor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Process first = new Process(7, 4, 6 );
+        Process second = new Process(7, 4, 6 );
+        
+    }
+    
+}
---
+++ Motor/Process.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package motor;
+
+/**
+ *
+ * @author henrisilver
+ */
+public class Process {
+    private int numberOfLines;
+    private int currentLine;
+    private int startOfCR;
+    private int endOfCR;
+    private boolean running;
+    private boolean inCritical;
+    
+    public Process(int num, int start, int end) {
+        numberOfLines = num;
+        currentLine = 0;
+        startOfCR = start;
+        endOfCR = end;
+        running = false;
+        inCritical = false;
+    }
+
+    public int getNumberOfLines() {
+        return numberOfLines;
+    }
+
+    public int getCurrentLine() {
+        return currentLine;
+    }
+
+    public int getStartOfCR() {
+        return startOfCR;
+    }
+
+    public int getEndOfCR() {
+        return endOfCR;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public boolean isInCritical() {
+        return inCritical;
+    }
+
+    public void setNumberOfLines(int numberOfLines) {
+        this.numberOfLines = numberOfLines;
+    }
+
+    public void setCurrentLine(int currentLine) {
+        this.currentLine = currentLine;
+    }
+
+    public void setStartOfCR(int startOfCR) {
+        this.startOfCR = startOfCR;
+    }
+
+    public void setEndOfCR(int endOfCR) {
+        this.endOfCR = endOfCR;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public void setInCritical(boolean inCritical) {
+        this.inCritical = inCritical;
+    }
+    
+}
